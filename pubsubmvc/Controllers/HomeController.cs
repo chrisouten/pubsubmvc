b48ac3873ab550969aeb40cb35f5399599e85f35
unknown <chris.outen@CHRISOPRE390.channeladvisor.com>
2012-09-26 17:31:38 -0400

    Trying out the redis on C# now

--- pubsubmvc/Controllers/HomeController.cs
+++ pubsubmvc/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ServiceStack.Redis;
+using System.Configuration;
 
 namespace pubsubmvc.Controllers
 {
@@ -12,7 +13,15 @@ namespace pubsubmvc.Controllers
 		public ActionResult Index()
 		{
 			ViewBag.Message = "Welcome to ASP.NET MVC!";
-
+			var redisUrl = ConfigurationManager.AppSettings.Get("REDISTOGO_URL");
+			var connectionUri = new Uri(redisUrl);
+			using (var redis = new RedisClient(connectionUri))
+			{
+				redis.Set("hello", "world");
+				var value = redis.Get<string>("hello");
+				ViewBag.Message = value;
+			}
+			
 			return View();
 		}
 
