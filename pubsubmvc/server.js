9c1820356ff8cdf2334c227c80c98c9504e12663
unknown <chris.outen@CHRISOPRE390.channeladvisor.com>
2012-09-26 15:53:28 -0400

    Initial commit

---
+++ pubsubmvc/server.js
@@ -0,0 +1,31 @@
+﻿var express = require('express'),
+    sio = require('socket.io'),
+    redis = require('redis');
+
+var client = redis.createClient();
+
+client.on("message", function (channel, message) {
+    client.socket.emit('update', message);
+});
+var app = express.createServer();
+app.listen(process.env.PORT || 80);
+
+var io = sio.listen(app);
+
+io.configure(function () {
+    io.set('transports', [
+                'xhr-polling',
+                'jsonp-polling',
+                'htmlfile'
+            ]);
+    io.set("polling duration", 10);
+});
+
+
+
+io.on('connection', function (socket) {
+    socket.on('connect', function (user) {
+        socket.user = user;
+        client.socket = socket;
+    });
+});
